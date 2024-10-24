cc0ea655cfee74c903f4b52db497e6b35e085947
The-yohanna <bradfordjohn998@gmail.com>
2024-10-24 19:38:17 +0300

    fix(main): Correctly import clerk components

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
-import { authMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-    ignoredRoutes: ['/api/webhooks(.*)'],
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks/clerk'])
+
+export default clerkMiddleware(async (auth, request) => {
+    if (!isPublicRoute(request)) {
+        await auth.protect()
+    }
 })
 
 export const config = {
