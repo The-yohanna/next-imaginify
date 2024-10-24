ba6d708cd62a8b8b17640a2a53631908058fb25f
The-yohanna <bradfordjohn998@gmail.com>
2024-10-24 18:43:43 +0300

    feat(main): First commit

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const AddTransformationsTypePage = () => {
+    return (
+        <div>AddTransformationsTypePage</div>
+    )
+}
+export default AddTransformationsTypePage
